a1731f0b22e8916a890f9804bff60f0811781f55
Jens Hassler <j.hassler@iwf.ch>
2017-05-28 12:44:17 +0200

    removed debugging statement

--- src/pages/routes/route-detail/route-detail.ts
+++ src/pages/routes/route-detail/route-detail.ts
@@ -27,7 +27,6 @@ export class RouteDetailPage {
     this.location = this.navParams.get('location');
     this.routeProvider.getItem(this.navParams.get('id')).subscribe(item => {
       this.route = item;
-      console.log(this.route);
       this.rating = this.ratingsProvider.getItem(item.ratingId);
     });
   }
